201060438fb36770e55fc9feb8779a5aac582750
rtfmkiesel <79413747+rtfmkiesel@users.noreply.github.com>
2025-01-31 10:49:23 +0100

    fix macOS sed by using perl

--- scripts/fetch.sh
+++ scripts/fetch.sh
@@ -6,6 +6,6 @@ curl -s -fsSL "$URL" -o /tmp/dist.zip && \
 unzip -o -q /tmp/dist.zip -d .
 
 # Adjust some URLs
-sed -i 's/\/assets/assets/g' NanoKVM-USB/index.html
-sed -i 's/\/sipeed\.ico/sipeed\.ico/g' NanoKVM-USB/index.html
-sed -i 's/\/sipeed\.ico/sipeed\.ico/g' NanoKVM-USB/assets/*.js
+perl -i -pe 's/\/assets/assets/g' NanoKVM-USB/index.html
+perl -i -pe 's/\/sipeed\.ico/sipeed\.ico/g' NanoKVM-USB/index.html
+perl -i -pe 's/\/sipeed\.ico/sipeed\.ico/g' NanoKVM-USB/assets/*.js
